7be54917dfda528ad78bd8d59df197e1a1b9b024
Aftab-1998 <alam41976@gmail.com>
2022-02-27 00:48:45 +0530

    [AFTAB5] Add; Added The method to retrieve salary of particular employee

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -12,7 +12,7 @@ create table employee_payroll
 );
 
 ---UC3 Create Employee payroll database service as a part of CURD operation---
-INSERT INTO employee_payroll (Name, Salary, Startdate) VALUES ('Mohammed', 45986.58, getdate());
+INSERT INTO employee_payroll (Name, Salary, Startdate) VALUES ('Mohammed', 45986.58, CAST('2018-02-02' as Date));
 INSERT INTO employee_payroll  VALUES ('Sajid', 95986.58, getdate());
 INSERT INTO employee_payroll  VALUES ('Sakil', 685986.58, getdate());
 INSERT INTO employee_payroll (Name, startdate) VALUES ('Roy', getdate());
@@ -20,3 +20,9 @@ INSERT INTO employee_payroll (Name, startdate) VALUES ('Roy', getdate());
 --- UC4 Ability to retrieve all the data that is added in payroll service database----
 select * from employee_payroll where Name= 'Sajid';
 select id,Name from employee_payroll where Name= 'Sajid';
+
+---UC5 Ability to retrieve salary data for a particular employee-----
+select salary from employee_payroll where Name= 'Sajid';
+select * from employee_payroll;
+select salary,Name from employee_payroll where startdate between CAST('2021-02-02' as Date) and getdate();
+
